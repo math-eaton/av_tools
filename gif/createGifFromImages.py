114b3768432e44db903555c956fced65539047c5
math-eaton <mheaton@ciesin.columbia.edu>
2023-10-03 12:36:25 -0400

    organize + gif framerate

--- gif/createGifFromImages.py
+++ gif/createGifFromImages.py
@@ -18,7 +18,7 @@ def create_gif(image_files, gif_path):
 
 # List of input directories
 image_folders = [
-    '/Users/matthewheaton/Documents/DOCENTS/video/hotnose',
+    '/Users/matthewheaton/Documents/DOCENTS/video/hotnose/inverted',
     # '/Users/matthewheaton/Documents/GitHub/imagery_scraper/output/processed_imagery/area',
     # '/Users/matthewheaton/Documents/GitHub/imagery_scraper/output/processed_imagery/point',
     # '/Users/matthewheaton/Documents/GitHub/imagery_scraper/output/processed_imagery/polyline',
