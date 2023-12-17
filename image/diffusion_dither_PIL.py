15b8079a4f45a0b7edb14a6d17a26a5cd87934bd
math-eaton <mheaton@ciesin.columbia.edu>
2023-12-17 16:46:33 -0500

    update PIL diffusion dither
    only output PNG and preserve aspect

--- image/diffusion_dither_PIL.py
+++ image/diffusion_dither_PIL.py
@@ -3,14 +3,28 @@ import numpy as np
 import os
 import argparse
 
-def process_image(filename, output_folder, size=(300, 300)):
+def calculate_new_size(image, dpi):
+    width, height = image.size
+    aspect_ratio = width / height
+    # Calculate the new size based on the specified dpi
+    new_height = dpi
+    new_width = int(aspect_ratio * new_height)
+    return (new_width, new_height)
+
+def process_image(filename, output_folder, dpi=300, specified_size=None):
     # Load the image
     image = Image.open(filename)
     print("Loading " + filename + "...")
 
-    # Resize the image (pre-dither) using nearest neighbor
-    image = image.resize(size, Image.NEAREST)
-    print("Resizing...")
+    # Determine the new size
+    if specified_size:
+        new_size = specified_size
+    else:
+        new_size = calculate_new_size(image, dpi)
+
+    # Resize the image while preserving aspect ratio
+    image.thumbnail(new_size, Image.NEAREST)
+    print(f"Resizing to {new_size} with aspect ratio preserved...")
 
     # Convert the image to grayscale
     image = image.convert('L')
@@ -33,10 +47,11 @@ def process_image(filename, output_folder, size=(300, 300)):
     image = Image.fromarray(data)
 
     # Resize the image (post-dither) using nearest neighbor
-    image = image.resize(size, Image.NEAREST)
+    image = image.resize(new_size, Image.NEAREST)
 
     # Save the image
-    output_filename = os.path.join(output_folder, os.path.basename(filename))
+    output_filename = os.path.splitext(os.path.basename(filename))[0] + ".png"
+    output_filename = os.path.join(output_folder, output_filename)
     image.save(output_filename)
     print("Saving " + output_filename)
 
@@ -44,14 +59,16 @@ def main():
     parser = argparse.ArgumentParser(description="Process images in a folder")
     parser.add_argument("input_folder", help="Path to the input folder containing images")
     parser.add_argument("output_folder", help="Path to the output folder for processed images")
-    parser.add_argument("--size", type=int, nargs=2, default=(300, 300), metavar=("WIDTH", "HEIGHT"),
-                        help="Size for resizing images (default: 300 300)")
+    parser.add_argument("--preserve_aspect_ratio", type=lambda x: (str(x).lower() == 'true'), default=True,
+                    help="Preserve aspect ratio of images (default: TRUE)")
+    parser.add_argument("--dpi", type=int, default=300, help="DPI for resizing images (default: 300)")
+    parser.add_argument("--size", type=int, nargs=2, metavar=("WIDTH", "HEIGHT"),
+                        help="Specific size for resizing images (overrides DPI setting)")
 
     args = parser.parse_args()
 
     input_folder = args.input_folder
     output_folder = args.output_folder
-    size = tuple(args.size)
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -61,10 +78,12 @@ def main():
 
     # Loop over all files
     for filename in files:
-        # Check if the file is an image
         if filename.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff')):
+            # Check if specific size is provided, otherwise set to None
+            specified_size = tuple(args.size) if args.size else None
+
             # Process the image
-            process_image(os.path.join(input_folder, filename), output_folder, size=size)
+            process_image(os.path.join(input_folder, filename), output_folder, dpi=args.dpi, specified_size=specified_size)
             print("Processed " + filename)
 
 if __name__ == "__main__":
